531af103661950744d3d160fa975d232c8ecad74
nic1207 <nic1207@gmail.com>
2017-06-03 11:33:38 +0800

    加入MyPluginPostProcessBuild 加入jsondotnet 加入memoryprofiletools 加入videorecording for plugins

---
+++ client/Assets/Editor/MyPluginPostProcessBuild.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class MyPluginPostProcessBuild
+{
+	[PostProcessBuild]
+	public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+	{
+		if ( buildTarget == BuildTarget.iOS )
+		{
+			// Get plist
+			string plistPath = pathToBuiltProject + "/Info.plist";
+			PlistDocument plist = new PlistDocument();
+			plist.ReadFromString(File.ReadAllText(plistPath));
+
+			// Get root
+			PlistElementDict rootDict = plist.root;
+
+			// background location useage key (new in iOS 8)
+			rootDict.SetString("NSMicrophoneUsageDescription", "xxx");
+			rootDict.SetString("NSCameraUsageDescription", "yyy");
+			rootDict.SetString ("UIBackgroundModes", "remote-notification");
+
+			// background modes
+			PlistElementArray bgModes = rootDict.CreateArray("UIBackgroundModes");
+			bgModes.AddString("location");
+			bgModes.AddString("fetch");
+
+			// Write to file
+			File.WriteAllText(plistPath, plist.WriteToString());
+		}
+	}
+}
---
+++ client/Assets/VideoRecoding/Scripts/VideoRecordingBridge.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class VideoRecordingBridge {
+
+string MessageReceivingGameObjectNameVariable;
+string MessageFromIosReceivingMethodNameVariable;
+//----------------------------------------------Show Recording-------------------------------------------------------------------
+	#if UNITY_IOS || UNITY_ANDROID
+	[DllImport("__Internal")]
+	private static extern void _initRecord ();
+	public static void InitRecord(){
+		#if !UNITY_EDITOR
+		_initRecord ();
+		#endif
+	}
+//----------------------------------------------Show Or Hide Recording-------------------------------------------------------------------
+// if isSowRecording is true then recording will be shown in recording view and if false then recording will be hideen
+	[DllImport("__Internal")]
+	private static extern void _tapRecord ();
+	public static void TapRecord(){
+		#if !UNITY_EDITOR && UNITY_IOS
+		_tapRecord ();
+		#endif
+	}
+	[DllImport("__Internal")]
+	private static extern void _startRecord ();
+	public static void StartRecord(){
+		#if !UNITY_EDITOR && UNITY_IOS
+		_startRecord ();
+		#endif
+	}
+	[DllImport("__Internal")]
+	private static extern void _stopRecord ();
+	public static void StopRecord(){
+		#if !UNITY_EDITOR && UNITY_IOS
+		_stopRecord ();
+		#endif
+	}
+#endif
+}

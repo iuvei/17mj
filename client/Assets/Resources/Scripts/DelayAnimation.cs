f7bc43f6bb0d406de642d41d607931fcb009e939
shiang-yu <ultrarider821@gmail.com>
2017-06-21 13:49:58 +0800

    Add Effect in lobby scene

---
+++ client/Assets/Resources/Scripts/DelayAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelayAnimation : MonoBehaviour {
+    public float _delaySec = 0f;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        if (anim && _delaySec > 0) {
+            anim.gameObject.SetActive(false);
+            Invoke("TurnOnAnim", _delaySec);
+        }
+            
+    }
+
+    private void TurnOnAnim() {
+        anim.gameObject.SetActive(true);
+    }
+}

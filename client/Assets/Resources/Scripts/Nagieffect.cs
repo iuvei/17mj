335ad5552e19d0ee4b309f72b18158c0f4cefd1c
shiang-yu <ultrarider821@gmail.com>
2017-06-27 18:39:37 +0800

    adjust room scene Nagi effect

---
+++ client/Assets/Resources/Scripts/Nagieffect.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nagieffect : MonoBehaviour {
+    public GameObject _chi;  //吃
+    public GameObject _pon;  //碰
+    public GameObject _gan;  //槓
+    public GameObject _tin;  //聽
+    public GameObject _hu;   //胡
+    public GameObject _pau;  //放槍
+
+    public enum NagiType {CHI,PON,GAN,TIN,HU,PAU}
+    private GameObject _ob;
+
+    void Start () {
+
+        if (!_chi || !_pon || !_gan || !_tin || !_hu || !_pau)
+            Debug.Log("No found correspond effect gameobject.");
+
+        gameObject.SetActive(false);
+	}
+
+    public void ShowNagi(NagiType _type) {
+        StopAllCoroutines();
+        HideAll();
+        gameObject.SetActive(false);
+
+        switch (_type) {
+            case NagiType.CHI:
+                _ob = _chi;
+                break;
+            case NagiType.PON:
+                _ob = _pon;
+                break;
+            case NagiType.GAN:
+                _ob = _gan;
+                break;
+            case NagiType.TIN:
+                _ob = _tin;
+                break;
+            case NagiType.HU:
+                _ob = _hu;
+                break;
+            case NagiType.PAU:
+                _ob = _pau;
+                break;
+        }
+        _ob.SetActive(true);
+        gameObject.SetActive(true);
+        StartCoroutine(HideNagi(_ob));
+    }
+
+    IEnumerator HideNagi(GameObject _ob) {
+        yield return new WaitForSeconds(2.3f);
+        _ob.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    private void HideAll() {
+        _chi.SetActive(false);
+        _pon.SetActive(false);
+        _gan.SetActive(false);
+        _tin.SetActive(false);
+        _hu.SetActive(false);
+        _pau.SetActive(false);
+    }
+}
---
+++ client/Assets/Resources/Scripts/TestCallNagi.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCallNagi : MonoBehaviour {
+    public bool _callChi = false;
+    public bool _callPon = false;
+    public bool _callGan = false;
+    public bool _callTin = false;
+    public bool _callHu = false;
+    public bool _callPau = false;
+
+    public Nagieffect nagiEffect;
+
+
+    void Update() {
+        if (_callChi) {
+            nagiEffect.ShowNagi(Nagieffect.NagiType.CHI);
+            _callChi = false;
+        }
+
+        if (_callPon) {
+            nagiEffect.ShowNagi(Nagieffect.NagiType.PON);
+            _callPon = false;
+        }
+            
+        if (_callGan) {
+            nagiEffect.ShowNagi(Nagieffect.NagiType.GAN);
+            _callGan = false;
+        }
+           
+        if (_callTin) {
+            nagiEffect.ShowNagi(Nagieffect.NagiType.TIN);
+            _callTin = false;
+        }
+           
+        if (_callHu) {
+            nagiEffect.ShowNagi(Nagieffect.NagiType.HU);
+            _callHu = false;
+        }
+
+        if (_callPau) {
+            nagiEffect.ShowNagi(Nagieffect.NagiType.PAU);
+            _callPau = false;
+        }
+            
+    }
+}

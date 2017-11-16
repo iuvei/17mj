b9db5102cb80caa8a5c8e9aa8702ea57633315e6
Orson <orson.hsueh@gmail.com>
2017-11-16 18:02:00 +0800

    Fix ios upload pic issue

---
+++ client/Assets/Plugins/iOS/Unimgpicker/UIImage+FixOrientation.m
@@ -0,0 +1,18 @@
+
+#import "UIImage+FixOrientation.h"
+
+@implementation UIImage(FixOrientation)
+
+- (UIImage *)imageWithFixedOrientation {
+    if (self.imageOrientation == UIImageOrientationUp) {
+        return self;
+    }
+
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, self.scale);
+    [self drawInRect:CGRectMake(0, 0, self.size.width, self.size.height)];
+    UIImage *fixedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return fixedImage;
+}
+
+@end
